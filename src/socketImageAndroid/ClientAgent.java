4c238ee819f0afdde68a2dfe4d2e78c1584f76a7
Guillaume Chanson <guillaume.chanson@dbmail.com>
2015-04-11 19:48:35 +0100

    Threadationage du client Android qui envoie désormais l'image bytée, et sa taille juste avant

--- src/socketImageAndroid/ClientAgent.java
+++ src/socketImageAndroid/ClientAgent.java
@@ -2,10 +2,7 @@ package socketImageAndroid;
 
 import org.apache.commons.codec.binary.Base64;
 
-import java.io.DataOutputStream;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.IOException;
+import java.io.*;
 import java.net.Socket;
 
 /**
@@ -44,27 +41,67 @@ public class ClientAgent implements Runnable {
      */
     @Override
     public void run() {
+        // Envoi
+        final Thread emission = new Thread(() -> {
+            try {
+                final File myFile = new File(Serveur.class.getResource("network.gif").getFile());
+                final FileInputStream imageInFile = new FileInputStream(myFile);
+                byte imageData[] = new byte[(int) myFile.length()];
+                int imageRead = imageInFile.read(imageData);
+
+                // Check size read
+                System.out.println("Size : " + (int) myFile.length() + " - Read : " + imageRead);
+
+                // Converting Image byte array into Base64 String
+                final String imageDataString = ClientAgent.encodeImage(imageData);
+                System.out.println("Encoded image size : " + imageDataString.length());
+
+                // Outputs the image data
+                final DataOutputStream outputStream = new DataOutputStream(this.client.getOutputStream());
+                outputStream.writeUTF(imageDataString);
+                outputStream.flush();
+
+                System.out.println("Sending...");
+                outputStream.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        });
+        emission.start();
+
+        // Réception
+        final Thread reception = new Thread(() -> {
+            try {
+                final DataInputStream inputStream = new DataInputStream(this.client.getInputStream());
+                final int byteImageSize = inputStream.readInt();
+                System.out.println("Image length is :" + byteImageSize);
+
+                final byte[] encodedImageByteArray = new byte[byteImageSize];
+                inputStream.readFully(encodedImageByteArray);
+
+                System.out.println("byte array length :" + encodedImageByteArray.length);
+                final byte[] imageByteArray = Base64.decodeBase64(encodedImageByteArray);
+
+                // Write a image byte array into file system
+                FileOutputStream imageOutFile = new FileOutputStream("/home/guillaume-chs/Bureau/image.jpg");
+                imageOutFile.write(imageByteArray);
+                imageOutFile.close();
+                inputStream.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        });
+        reception.start();
+
+        // Then close the connexion
         try {
-            final File myFile = new File(Serveur.class.getResource("network.gif").getFile());
-            final FileInputStream imageInFile = new FileInputStream(myFile);
-            byte imageData[] = new byte[(int) myFile.length()];
-            int imageRead = imageInFile.read(imageData);
-
-            // Check size read
-            System.out.println("Size : " + (int) myFile.length() + " - Read : " + imageRead);
-
-            // Converting Image byte array into Base64 String
-            final String imageDataString = ClientAgent.encodeImage(imageData);
-            System.out.println("Encoded image size : " + imageDataString.length());
-
-            // Outputs the image data
-            final DataOutputStream outputStream = new DataOutputStream(client.getOutputStream());
-            outputStream.writeUTF(imageDataString);
-            outputStream.flush();
-
-            System.out.println("Sending...");
-
-            // Then close the connexion
+            while (!(reception.isInterrupted() && emission.isInterrupted())) {
+                try {
+                    Thread.sleep(10L);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
             client.close();
         } catch (IOException e) {
             e.printStackTrace();
